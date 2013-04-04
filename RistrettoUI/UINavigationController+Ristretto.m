6beafe47d08cf4ed63f7faa28d508611f537bf15
Marco Meschini <marco@futureworkshops.com>
2013-04-04 11:25:08 +0100

    Restore navController category

--- RistrettoUI/UINavigationController+Ristretto.m
+++ RistrettoUI/UINavigationController+Ristretto.m
@@ -10,10 +10,17 @@
 #import "UIImage+Ristretto.h"
 #import "UIFont+Ristretto.h"
 #import "UIColor+Ristretto.h"
+#import "RistrettoTableViewController.h"
+#import "UIImage+FWTBlock.h"
 
 @implementation UINavigationController (Ristretto)
 
 + (id)Ristretto_navigationControllerWithRootViewController:(UIViewController *)viewController
+{
+    return [[self class] Ristretto_navigationControllerWithRootViewController:viewController defaultHeaderEnabled:NO];
+}
+
++ (id)Ristretto_navigationControllerWithRootViewController:(UIViewController *)viewController defaultHeaderEnabled:(BOOL)enabled
 {
     UINavigationController *nc = [[[UINavigationController alloc] initWithRootViewController:viewController] autorelease];
     
@@ -24,6 +31,34 @@
                                       UITextAttributeTextShadowColor : [UIColor clearColor],
                                       UITextAttributeFont : [UIFont Ristretto_lightFontOfSize:22]};
     [nc.navigationBar setTitleTextAttributes:titleAttributes];
+    
+    //
+    NSDictionary *infoDictionary = [[NSBundle mainBundle] infoDictionary];
+    NSString *title = [infoDictionary valueForKey:@"CFBundleDisplayName"];
+    viewController.title = title;
+    
+    //
+    if (enabled && [viewController isKindOfClass:[RistrettoTableViewController class]])
+    {
+        //
+        UIView *headerView = [[[UIView alloc] initWithFrame:(CGRect){.0f, .0f, 240.0f, 200.0f}] autorelease];
+        [(RistrettoTableViewController *)viewController setTableHeaderView:headerView insets:(UIEdgeInsets){58.0f, 40.0f, 45.0f, 40.0f}];
+        
+        UIImageView *iconView = [[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"icon_project.png"]] autorelease];
+        iconView.center = CGPointMake(CGRectGetMidX(headerView.bounds), iconView.center.y);
+        [headerView addSubview:iconView];
+        
+        UILabel *label = [[[UILabel alloc] init] autorelease];
+        label.text = @"Choose a sample:";
+        label.font = [UIFont Ristretto_lightFontOfSize:18.0f];
+        label.textColor = [UIColor Ristretto_darkGrayColor];
+        [label sizeToFit];
+        label.center = CGPointMake(CGRectGetMidX(headerView.bounds), CGRectGetHeight(headerView.bounds)-CGRectGetHeight(label.bounds)*.5f);
+        [headerView addSubview:label];
+    }
+    
+    UIImage *emptyImage = [UIImage fwt_imageWithSize:(CGSize){1.0f, 1.0f} block:NULL];
+    [nc.toolbar setBackgroundImage:emptyImage forToolbarPosition:UIToolbarPositionAny barMetrics:UIBarMetricsDefault];
     
     return nc;
 }
